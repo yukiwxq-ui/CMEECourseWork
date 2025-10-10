de13a517d62716ef5f9dab2a0c47e04e522c02c6
Yuki Wu <xiaoqi.wu22@imperial.ac.uk>
2025-10-10 02:37:52 +0100

    pushing CompileLaTeX.sh

---
+++ week1/code/CompileLaTeX.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: bash CompileLaTeX.sh <filename.tex>"
+  exit 1
+fi
+
+# Extract the filename without extension
+FILENAME=$(basename "$1" .tex)
+
+# Compile LaTeX document
+pdflatex "$FILENAME.tex"
+bibtex "$FILENAME"
+pdflatex "$FILENAME.tex"
+pdflatex "$FILENAME.tex"
+
+# Open the PDF
+evince "$FILENAME.pdf" &
+
+# Cleanup auxiliary files
+rm -f *.aux *.log *.bbl *.blg
